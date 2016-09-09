eaf79983afbbfd1675aff2a48d3ae6bb6bee8d49
lushijie <lushijie1218@126.com>
2016-09-09 10:02:12 +0800

    删除无用路由配置

--- resource/js/index.jsx
+++ resource/js/index.jsx
@@ -23,7 +23,6 @@ import 'resource/css/index.scss';
 //                {/* 跳转 /inbox/messages/:id 到 /messages/:id 来兼容旧的url */}
 //                {/*<Redirect from="messages/:id" to="/messages/:id" /> */}
 //                <Route path="messages/:id" component={Message} />
-
 //              </Route>
 //          </Route>
 //      </Router>
@@ -32,64 +31,12 @@ import 'resource/css/index.scss';
 
 
 // 路由实现方式2
-// const routeConfig = [
-//   { path: '/',
-//     component: App,
-//     indexRoute: { component: Dashboard },
-//     childRoutes: [
-//       { path: 'about', component: About },
-//       { path: 'inbox',
-//         component: Inbox,
-//         childRoutes: [
-//           {
-//              path: 'messages/:id', component: Message
-//           }
-//         ]
-//       }
-//     ]
-//   }
-// ]
-
-//路由实现方式2_URL路径修改
-// const routeConfig = [
-//  {
-//      path: '/',
-//      component: App,
-//      indexRoute: { component: Dashboard },
-//      childRoutes: [
-//        { path: 'about', component: About },
-//        { path: 'inbox',
-//          component: Inbox,
-//          childRoutes: [
-//            { path: '/messages/:id', component: Message },
-//            { path: 'messages/:id',
-//              onEnter: function (nextState, replace) {
-//                 replace('/messages/' + nextState.params.id)//路由替换
-//              },//onEnter hook会从最外层的父路由开始直到最下层子路由结束
-//              onLeave: function () {
-//              }//onLeave hook 会在所有将离开的路由中触发，从最下层的子路由开始直到最外层父路由结束
-//            }
-//          ]
-//        }
-//      ]
-//  },
-//  {
-//    path: '*',
-//    component: NotFound,
-//  }
-// ]
-
-// render(<Router history={browserHistory} routes={routeConfig} />, document.getElementById('app2'))
-
-
-
 //路由实现方式——动态路由
-
 const routeConfig = [
     {
         path: '/',
         component: App,
-        indexRoute: { component: Dashboard },
+        indexRoute: { component: Dashboard },//index路由加载项
         childRoutes: [
             {
                 path: 'about',
