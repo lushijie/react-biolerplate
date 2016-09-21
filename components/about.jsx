cedac8fc481c41b9dd452b555282e8b96f3ad809
lushijie <lushijie1218@126.com>
2016-09-21 15:57:25 +0800

    this.context.router

--- components/about.jsx
+++ components/about.jsx
@@ -1,7 +1,27 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import { withRouter } from 'react-router';
 
 export default class extends Component {
+
+	// static contextTypes: {
+	//     router: React.PropTypes.func.isRequired
+	// }
+	static contextTypes = {
+	  router: React.PropTypes.object.isRequired,
+	  location: React.PropTypes.object.isRequired
+	}
+
+	state = {
+	  // 初始化state,替代原getInitialState, 注意前面没有static
+	  unsaved: true
+	}
+
+	componentDidMount() {
+		console.log('this.props->', this.props);
+	    console.log('this.context.router', this.context.router);
+	}
+
 	render() {
 		return (
 			<div>
