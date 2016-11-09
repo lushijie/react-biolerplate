99b5d5b9414708c32e1b09b024eef898d513a331
lushijie <lushijie1218@126.com>
2016-11-09 22:18:44 +0800

    重命名

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,14 @@
 * @Author: lushijie
 * @Date:   2016-09-28 17:19:43
 * @Last Modified by:   lushijie
-* @Last Modified time: 2016-11-09 22:08:51
+* @Last Modified time: 2016-11-09 22:18:09
 */
 
 import App from 'components/app.jsx';
 import AppDashboard from 'components/app_dashboard.jsx';
 import NotFound from 'components/common/404.jsx';
-import indexInboxRoute from 'routes/index-inbox.js';
-import indexAboutRoute from 'routes/index-about.js';
+import indexInboxRoute from 'routes/index_inbox.js';
+import indexAboutRoute from 'routes/index_outbox.js';
 
 // 配置路由实现方式——动态路由
 export default [
