442c4a2b70912d0ce750e8596d4438b5cc9d3f3e
lushijie <lushijie1218@126.com>
2016-11-11 10:11:00 +0800

    readme

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,8 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import { Router, Route, IndexRoute, Link, IndexLink, browserHistory} from 'react-router';
-
-import './index.scss';
 import indexRoute from 'routes';
-
-// 路由实现方式1
-//render(
-// (
-//     <Router history={hashHistory}>
-//         <Route path="/" component={App}>
-//             <IndexRoute component={Dashboard} />
-//             <Route path="about" component={About} />
-//             <Route path="inbox" component={Inbox}>
-//               {/* 使用 /messages/:id 替换 messages/:id */}
-//               {/* <Route path="/messages/:id" component={Message} /> */}
-//               {/* 跳转 /inbox/messages/:id 到 /messages/:id 来兼容旧的url */}
-//               {/*<Redirect from="messages/:id" to="/messages/:id" /> */}
-//               <Route path="messages/:id" component={Message} />
-//             </Route>
-//         </Route>
-//     </Router>
-// ),document.getElementById('app2')
-//);
+import './index.scss';
 
 render(
   <Router history={browserHistory} routes={indexRoute} />,
---
+++ src/readme.md
@@ -0,0 +1,18 @@
+// 路由实现方式2（个人不喜欢这种方式）
+//render(
+// (
+//     <Router history={hashHistory}>
+//         <Route path="/" component={App}>
+//             <IndexRoute component={Dashboard} />
+//             <Route path="about" component={About} />
+//             <Route path="inbox" component={Inbox}>
+//               {/* 使用 /messages/:id 替换 messages/:id */}
+//               {/* <Route path="/messages/:id" component={Message} /> */}
+//               {/* 跳转 /inbox/messages/:id 到 /messages/:id 来兼容旧的url */}
+//               {/*<Redirect from="messages/:id" to="/messages/:id" /> */}
+//               <Route path="messages/:id" component={Message} />
+//             </Route>
+//         </Route>
+//     </Router>
+// ),document.getElementById('app2')
+//);
