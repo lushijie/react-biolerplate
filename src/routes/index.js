ea2a4429b27ea3ba0951648829644522fe7a9c4a
lushijie <lushijie1218@126.com>
2016-11-27 22:15:11 +0800

    fix name

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,14 @@
 * @Author: lushijie
 * @Date:   2016-09-28 17:19:43
 * @Last Modified by:   lushijie
-* @Last Modified time: 2016-11-09 22:22:48
+* @Last Modified time: 2016-11-27 22:14:58
 */
 
 import App from 'components/index/app';
 import AppDashboard from 'components/index/dashboard';
 import NotFound from 'components/common/404';
 import indexInboxRoute from 'routes/index_inbox';
-import indexAboutRoute from 'routes/index_outbox';
+import indexOutboxRoute from 'routes/index_outbox';
 
 // 配置路由实现方式——动态路由
 export default [
@@ -18,7 +18,7 @@ export default [
         component: App,
         indexRoute: { component: AppDashboard },//index路由加载项
         childRoutes: [
-            indexAboutRoute,
+            indexOutboxRoute,
             indexInboxRoute
         ]
     },
