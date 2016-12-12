1ad7141061fa249118634cd5e3c27452c0e9c8fb
lushijie <lushijie1218@126.com>
2016-12-12 10:33:19 +0800

    fix

--- webpack.config.setting.js
+++ webpack.config.setting.js
@@ -2,7 +2,7 @@
  * @Author: lushijie
  * @Date:   2016-11-11 17:20:12
  * @Last Modified by:   lushijie
- * @Last Modified time: 2016-12-12 10:25:06
+ * @Last Modified time: 2016-12-12 10:30:23
  */
 var path = require('path');
 var moment = require('moment');
@@ -14,7 +14,7 @@ var htmlPluginOptions = {
   hash: true,
   inject: false,
   template: path.resolve(__dirname, 'src/index.html'),
-  favicon: path.resolve(__dirname, 'src/resource/images/favicon.ico'),
+  favicon: './src/resource/images/favicon.ico',
   minify: {
     removeComments: false,
     collapseWhitespace: false,
