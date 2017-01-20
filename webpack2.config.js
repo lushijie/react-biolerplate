8b889a4893b8c8d76b64470a0e57718d7ef7f0d5
lushijie <lushijie1218@126.com>
2017-01-20 17:51:04 +0800

    open gzip

--- webpack2.config.js
+++ webpack2.config.js
@@ -2,7 +2,7 @@
 * @Author: lushijie
 * @Date:   2017-01-04 17:36:43
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-01-20 17:39:59
+* @Last Modified time: 2017-01-20 17:49:46
 */
 var webpack = require('webpack')
 var path = require('path')
@@ -134,6 +134,7 @@ module.exports = {
       cached: false,
       colors: true
     },
+    compress: true,
     contentBase: '.',
     port: 5050,
     host: '0.0.0.0',
