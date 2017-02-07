039907935b290599871ab337afe410e91d52e2da
lushijie <lushijie1218@126.com>
2017-02-07 18:35:02 +0800

    no order stories

--- src/components/stories.js
+++ src/components/stories.js
@@ -2,7 +2,15 @@
 * @Author: lushijie
 * @Date:   2017-02-07 14:29:48
 * @Last Modified by:   lushijie
-* @Last Modified time: 2017-02-07 17:45:40
+* @Last Modified time: 2017-02-07 18:31:51
 */
-require('./Welcome/story')
-require('./Button/story')
+
+let req = require.context('./', true, /story\.js$/);
+req.keys().forEach(function(key){
+  req(key);
+});
+
+// require('./Welcome/story')
+// require('./Button/story')
+
+
